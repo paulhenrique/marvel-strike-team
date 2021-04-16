cd963e83d00eef7dc89e6e70341b33f4f11e1d01
Paulo Henrique <phvcandido@gmail.com>
2021-04-16 07:50:21 -0300

    Ajustando containeres

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Header from '../../components/Header';
+import './style.scss';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <div id="characters">
+        <div className="container">
+          <section>
+            <h1>Characters</h1>
+            <p># results</p>
+          </section>
+        </div>
+      </div>
     </>
   );
 }
