08ef91b533d5073d51823de9af846a61e05eef3b
Paulo Henrique <phvcandido@gmail.com>
2021-04-22 10:58:44 -0300

    Adicionando Parallax

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import searchIcon from '../../assets/img/search.svg';
 import backgroundImage from '../../assets/img/background-header.png';
 import './style.scss';
 import { useHistory } from 'react-router';
+import { Parallax } from 'react-parallax';
 
 interface AuxProps {
   hero?: string;
@@ -16,33 +17,36 @@ const Header: React.FC<AuxProps> = (props) => {
 
   return (
     <>
-      <header style={{ backgroundImage: `url(${backgroundImage})` }}>
-        {
-          !props.team ? (
-            <div className="headerHome">
-              <h1 className="headerTitle">Explore the most powerful characters in Marvel</h1>
-              <fieldset>
-                <input
-                  value={searchTerm}
-                  onChange={event => setSearchTerm(event.target.value)}
-                  type="text"
-                  placeholder="Type in a character name"
-                />
-                <button onClick={() => history.push(`/${searchTerm}`)} type="submit">
-                  <img src={searchIcon} alt="ícone de lupa para pesquisa" />
-                </button>
-              </fieldset>
-            </div>
-          ) : (
-            <div className="headerHero">
-              <div className="container">
-                <h1 className="headerTitle">{props.hero !== '' ? props.hero : ''}</h1>
-              </div>
-            </div>
-          )
+      <Parallax bgImage={backgroundImage} strength={250} >
+        <header >
 
-        }
-      </header>
+          {
+            !props.team ? (
+              <div className="headerHome">
+                <h1 className="headerTitle">Explore the most powerful characters in Marvel</h1>
+                <fieldset>
+                  <input
+                    value={searchTerm}
+                    onChange={event => setSearchTerm(event.target.value)}
+                    type="text"
+                    placeholder="Type in a character name"
+                  />
+                  <button onClick={() => history.push(`/${searchTerm}`)} type="submit">
+                    <img src={searchIcon} alt="ícone de lupa para pesquisa" />
+                  </button>
+                </fieldset>
+              </div>
+            ) : (
+              <div className="headerHero">
+                <div className="container">
+                  <h1 className="headerTitle">{props.hero !== '' ? props.hero : ''}</h1>
+                </div>
+              </div>
+            )
+
+          }
+        </header>
+      </Parallax>
 
     </>
   );
